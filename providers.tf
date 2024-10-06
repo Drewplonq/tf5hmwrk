4c89cafcf847001b8a14fbef65a99c06a4daf9bf
Drewplonq <it@goplonq.com>
2024-10-06 23:08:17 +0300

    Add files via upload

--- providers.tf
+++ providers.tf
@@ -1,4 +1,16 @@
 terraform {
+backend "s3" {
+    endpoints = {s3 = "https://storage.yandexcloud.net"}
+    bucket = "net0l0gy"
+    region = "ru-central1-a"
+    key = "terraform.tfstate"
+    skip_region_validation = true
+    skip_credentials_validation = true
+    skip_requesting_account_id = true
+    skip_s3_checksum = true
+    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gmpma17e5otjoaje18/etnpmve3aocchc79qn7t"
+    dynamodb_table = "tfstate-lock"
+}
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
@@ -8,9 +20,8 @@ terraform {
 }
 
 provider "yandex" {
-  #token     = var.token
+  token     = var.token
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = var.default_zone
-  service_account_key_file = file("authorized_key.json")
- }
+}
