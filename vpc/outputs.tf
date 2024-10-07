f626f6a257f4acf4e3e56690f4a7ac11f687d4b0
Drewplonq <it@goplonq.com>
2024-10-07 14:55:16 +0300

    Delete vpc directory

--- vpc/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "vpc_network"{
-    value=yandex_vpc_network.network
-    description="vpc network"
-}
-output "vpc_subnet"{
-    value=yandex_vpc_subnet.subnet
-    description="vpc subnet"
-}
